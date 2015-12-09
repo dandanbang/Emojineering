15127848825aea22f69407ccc500cb9d03a2f69e
hatsy <hr@airbears2-10-142-139-65.airbears2.1918.berkeley.edu>
2015-12-08 19:37:05 -0800

    Implemented retweets cleaning

--- 2015-12-06-hr-data-cleaning.py
+++ 2015-12-06-hr-data-cleaning.py
@@ -27,7 +27,7 @@
 # "@letwerkaaaaa: “@Palmira_0: HAHA WHEN I WAS LITTLE I WAS FAT AS FUCK:joy:” Same I was so fat that they thought my vagina was a dick" LMFAO
 #     - For now will leave them here. We might have to consider while training if it is retweeted content or original content. Actually impacts the single response rate, ie when people just add an emoji to a tweet (as in the emoji is the sole content) 
 
-# In[1]:
+# In[88]:
 
 import json
 import numpy as np
@@ -38,14 +38,18 @@ import pandas as pd
 pd.options.display.max_colwidth = 140
 import nltk
 import re
+from IPython.display import display
 
 
-# In[15]:
+# In[214]:
 
-with open('./data/tweets_training.json','r') as f:
-    tweets_df = pd.DataFrame(json.load(f))
-clean_tweets_df = pd.DataFrame(tweets_df["text"])
-# del tweets_df
+def loader(filename):
+    with open(filename,'r') as f:
+        df = pd.DataFrame(json.load(f))
+    df.reset_index(inplace=True, drop=True)
+    return df
+tweets_df = loader('./data/tweets_training.json')
+clean_tweets_df = tweets_df.ix[:, ["text", "id"]]
 
 
 # ### Replace @ handles with hdl
@@ -54,7 +58,7 @@ clean_tweets_df = pd.DataFrame(tweets_df["text"])
 # 
 # Best to replace handles with "\ hdl\ ", so for tokenization it will be easier to identify as a word. 
 
-# In[16]:
+# In[215]:
 
 def cleanHandle(df):
     """ Replace in-place handles with hdl keyword
@@ -67,7 +71,7 @@ def cleanHandle(df):
     return
 
 cleanHandle(clean_tweets_df)
-clean_tweets_df.sample(10)
+# clean_tweets_df.sample(10)
 
 
 # ### Replace URLs with url
@@ -76,25 +80,26 @@ clean_tweets_df.sample(10)
 # 
 # keyword url is used only 4 times in dataset, no risk of confusion
 
-# In[17]:
+# In[216]:
 
 def cleanURL(df):
     """ Replace in-place URLs with url keyword
     
     Returns None
     """
-    pattern = r"http://\S+"
+#     pattern = r"http://\S+"
+    pattern = r'(?:http://|https://|www.)[^“”"\' ]+' # From http://stackoverflow.com/questions/7679970/python-regular-expression-nltk-website-extraction
     print("{} urls replaced".format(np.sum(clean_tweets_df.text.str.contains(pattern).values)))
     df.text = df.text.str.replace(pattern, " url ")
     return
 
 cleanURL(clean_tweets_df)
-clean_tweets_df.sample(10)
+# clean_tweets_df.sample(100)
 
 
 # ### Convert emoticons to emojis
 
-# In[22]:
+# In[92]:
 
 # Based on:
 # https://slack.zendesk.com/hc/en-us/articles/202931348-Emoji-and-emoticons
@@ -143,18 +148,113 @@ def convertEmoticon(df):
 convertEmoticon(clean_tweets_df)
 
 
-# ### Explore retweets
+# ### Split retweets into user content and retweeted content
 
-# In[10]:
+# In[326]:
 
-pattern = r"""(?:\W|^)RT(?:[ \":“]|$)| # Retweets with RT keyword
-            [\"“]\s*hdl                # Retweets with quotes"""
-temp = clean_tweets_df.text.str.contains(pattern, flags=re.X)
-print("There are {} retweets".format(np.sum(temp.values)))
-clean_tweets_df[temp].sample(10)
+def splitRetweets(df):
+    """ Extract retweets with the RT keyword"""
+    pattern = r"""(.*(?:\W|^))(RT(?:\ ?[\":“]|$).*) # Retweets with RT keyword"""
+    retweets = pd.DataFrame(df.text.str.extract(pattern, flags=re.X))
+    retweets.columns = ["text", "retweet"]
+    non_null_idxs = retweets.retweet.notnull()
+    df.loc[non_null_idxs,["text"]] = retweets.text[non_null_idxs]
+    df["retweet"] = retweets.retweet
+    return len(non_null_idxs)
+
+
+# In[335]:
+
+def splitQuotes(df):
+    """ Extract retweets in quote format.
+    See http://support.gnip.com/articles/identifying-and-understanding-retweets.html
+    """
+    pattern = r"(.*?)((?:([\"\'])|(?:(“))|‘)\s*hdl.*(?(3)\3|(?(4)”|’)))(.*)" #Pattern to match quote, possibly nested
+    retweets = pd.DataFrame(df.text.str.extract(pattern, flags=re.X)[[0,1,4]])
+    retweets.columns = ["text_before", "retweet", "text_after"]
+    non_null_idxs = retweets.retweet.notnull()
+    retweets["text"] = retweets.loc[non_null_idxs, ["text_before", "text_after"]].apply(lambda x: " ".join(x), axis=1)
+    df.loc[non_null_idxs,["text"]] = retweets.text[non_null_idxs].copy()
+    df["retweet"] = retweets.retweet.copy()
+    return len(retweets[non_null_idxs])
+
+
+# In[328]:
+
+def cleanRetweets(df):
+    """ Remove from the text column the retweet column and add to seperate column "retweet". 
+    
+    Returns None
+    """
+    nsplits = 0
+    nsplits += splitRetweets(df)
+    nsplits += splitQuotes(df) # Need to maintain that order
+    print("{} retweets processed".format(nsplits))
+    return
+
+
+# In[329]:
+
+cleanRetweets(clean_tweets_df)
+display(clean_tweets_df.dropna(subset = ["retweet"]))
 
 
 # ###Twitter hashtags
+
+# In[ ]:
+
+
+
+
+# ##Supportive functions
+
+# #Functions to extract only emoji or only text from input
+
+# In[4]:
+
+try:
+    # Wide UCS-4 build
+    highpoints = re.compile(u'['
+        u'\U0001F300-\U0001F64F'
+        u'\U0001F680-\U0001F6FF'
+        u'\u2600-\u26FF\u2700-\u27BF]+', 
+        re.UNICODE)
+except re.error:
+    # Narrow UCS-2 build
+    highpoints = re.compile(u'('
+        u'\ud83c[\udf00-\udfff]|'
+        u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+        u'[\u2600-\u26FF\u2700-\u27BF])+', 
+        re.UNICODE)
+
+
+# In[ ]:
+
+# Functions to check whether there's an emoji in the text, return 1 if true, 0 if false
+def is_emoji(text):
+    if highpoints.search(text):
+        return 1
+    else:
+        return 0
+
+
+# In[1]:
+
+def emojiExtract(sent):
+    return [word for word in tok.tokenize(sent) if is_emoji(word) == 1]
+
+def textExtract(sent):
+    return [word for word in tok.tokenize(sent) if is_emoji(word) == 0]
+
+
+# ##Functions to Create Two New Columns [Text Only] & [Emoji Only]
+
+# In[2]:
+
+def textEmojiOnly(df):
+    df['Emoji'] = [emojiExtract(word) for word in df.text]
+    df['only_Text'] = [textExtract(word) for word in df.text]
+
 
 # In[ ]:
 
