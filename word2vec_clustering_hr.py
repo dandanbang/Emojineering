a8f35e671739bafb690112f1f6607ef9b546e548
hatsy <hr@That.hsd1.ca.comcast.net>
2015-12-12 16:34:04 -0800

    Finished word2vec hierarchical clustering

--- word2vec_clustering_hr.py
+++ word2vec_clustering_hr.py
@@ -5,7 +5,7 @@
 # 
 # Inspired from data_exploration_dc.ipynb (but no functions so I couldn't import it! ;))
 
-# In[3]:
+# In[2]:
 
 import gensim
 from gensim.models import Word2Vec
@@ -31,38 +31,51 @@ get_ipython().magic('matplotlib inline')
 
 # ## Load files
 
-# In[32]:
+# In[29]:
 
 tweet_df = loader("./data/tweets_training_clean.json")
 emojis_df = loader("./data/emoji_webscraped_expanded.json")
+
+
+# In[30]:
+
+def convertEmojis(df):
+    """Converts emojis df to printable format """
+    emojis = list(map(lambda x: bytes("{}{}".format(*x), 'ascii').decode('unicode-escape'), zip(list(df.byteCode1), list(df.byteCode2)))) 
+    return emojis
+
+
+# In[31]:
+
+emojis_df["emojis"] = convertEmojis(emojis_df)
 
 
 # ## Training word2vec model
 
 # When defining the word2vec model, with a window of 5 (default) we are only considering emojis that cooccur close by. Which makes sense. We could even use a smaller window. 
 
-# In[16]:
+# In[4]:
 
 tok = src.happyfuntokenizing.Tokenizer(preserve_case=False)
 tweet_df["tokens"] = [list(tok.tokenize(tweet)) for tweet in tweet_df.text]
 
 
-# In[17]:
+# In[ ]:
 
 # To reduce size, train only on tweets with emojis
-emoji_model = gensim.models.Word2Vec(list(tweet_df.tokens), window=10) # Does the tokenizer work well on flags? 
+# emoji_model = gensim.models.Word2Vec(list(tweet_df.tokens), window=10, size=200, workers=4, min_count=100) # Does the tokenizer work well on flags? 
 # emoji_model.save('emoji.embedding')
 emoji_model = gensim.models.Word2Vec.load('emoji.embedding')
 
 
-# In[18]:
+# In[10]:
 
-emoji_model.most_similar(positive = ['\U0001F601'], negative= [])
+emoji_model.most_similar(positive = ['🍻'], negative= [])
 
 
 # ## Hierarchical clustering
 
-# In[48]:
+# In[50]:
 
 # Subsetting to faces
 def subset_annotations(df):
@@ -72,24 +85,26 @@ def subset_annotations(df):
     df_face_person = df.iloc[index_face_person]
     return df_face_person
 
-emojis_df = subset_annotations(emojis_df)
-print(len(emojis_df))
+
+# In[51]:
+
+def subset_present(df):
+    # Select only emojis that are in our model (ie in the corpus)
+    return df[df["emojis"].map(lambda x: x in emoji_model.vocab.keys())]
 
 
-# In[57]:
+# In[62]:
 
-def convertEmojis(df):
-    """Converts emojis df to printable format """
-    emojis = list(map(lambda x: bytes("{}{}".format(*x), 'ascii').decode('unicode-escape'), zip(list(df.byteCode1), list(df.byteCode2)))) 
-    return emojis
+# Subset to faces present
+# emojis_df_sub = subset_present(subset_annotations(emojis_df))
+emojis_df_sub = subset_present(emojis_df)
+print(len(emojis_df_sub))
 
 
-# In[58]:
+# In[63]:
 
 # Calculate distances
-emojis = convertEmojis(emojis_df)
-combinations = itertools.combinations(emojis, 2)
-
+combinations = itertools.combinations(emojis_df_sub.emojis, 2)
 similarities = []
 for combination in combinations:
     try:
@@ -106,16 +121,16 @@ distances = 1 - np.arccos(similarities) / np.pi # From https://en.wikipedia.org/
 
 # #### Using Scipy linkage and dandrogram
 
-# In[59]:
+# In[64]:
 
 hc_linkage = linkage(distances, method="complete")
 
 
-# In[60]:
+# In[65]:
 
 p = 121 # Number of clusters at which to truncate
 fig, ax = plt.subplots(figsize=(15, 0.2*p)) # set size
-R = dendrogram(hc_linkage, orientation="right", labels=emojis, truncate_mode="lastp", p=p, get_leaves=True, show_leaf_counts=True, show_contracted=False)
+R = dendrogram(hc_linkage, orientation="right", labels=list(emojis_df_sub.emojis), truncate_mode="lastp", p=p, get_leaves=True, show_leaf_counts=True, show_contracted=False)
 
 plt.tick_params(    axis= 'x',          # changes apply to the x-axis
     which='both',      # both major and minor ticks are affected
@@ -128,20 +143,20 @@ ax.xaxis.grid(True, which='minor')
 # ax.set_yticklabels(a.get_yticks(), emoji_font)
 
 
-# In[61]:
+# In[78]:
 
 from scipy.cluster.hierarchy import fcluster
-k=11
+k=20
 categories = fcluster(hc_linkage, k, criterion='maxclust')
-emojis_df["category"] = categories
+emojis_df_sub.loc[:,"category"] = categories
 
 
-# In[62]:
+# In[79]:
 
 for category in range(1, k+1):
     print("Category {}".format(category))
-    emojis_cat = emojis_df[emojis_df["category"] == category]
-    for emoji in convertEmojis(emojis_cat):
+    emojis_cat = emojis_df_sub[emojis_df_sub["category"] == category]
+    for emoji in emojis_cat.emojis:
         print(emoji, end="")
     print("\n")
 
