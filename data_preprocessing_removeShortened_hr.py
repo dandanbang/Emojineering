fd934f8ec9f17b1b18a3dca79f1d5bc1c99bdc12
hatsy <hadrien.renold@gmail.com>
2015-12-14 21:51:17 -0800

    Minor changes

--- data_preprocessing_removeShortened_hr.py
+++ data_preprocessing_removeShortened_hr.py
@@ -1,10 +1,12 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 import nltk
-from data_cleaning_hr import loader
+import pandas as pd
+import json
+
 def loader(filename):
     """ Load tweets from filename. Resets the index. Returns the loaded data frame"""
     with open(filename,'r') as f:
@@ -13,34 +15,34 @@ def loader(filename):
     return df
 
 
-# In[ ]:
+# In[7]:
 
 tweets_df = loader("./data/tweets_test_clean_preprocessing.json")
 
 
-# In[ ]:
+# In[8]:
 
 tweets_df.head()
 len(tweets_df)
 
 
-# In[ ]:
+# In[9]:
 
 # Filter tweets by those of more than 50 characters 
 tweets_df_filtered = tweets_df[tweets_df.only_text_splithashtag.apply(lambda x: len(x)) > 50]
 
 
-# In[ ]:
+# In[10]:
 
 tweets_df_filtered.head()
 
 
-# In[ ]:
+# In[11]:
 
 sum(tweets_df_filtered.only_emoji.apply(lambda x: len(x) > 0))
 
 
-# In[ ]:
+# In[12]:
 
 temp = tweets_df_filtered.only_emoji.values.flatten().tolist()
 import itertools
@@ -49,7 +51,7 @@ fdist = nltk.FreqDist(chain)
 fdist.most_common()
 
 
-# In[ ]:
+# In[13]:
 
 tweets_df_filtered.to_json('./data/tweets_test_clean_preprocessingv2.json', force_ascii=False)
 
